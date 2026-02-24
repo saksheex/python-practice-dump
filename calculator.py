ee1881c353395e3e9ab15e5bd59efe63c015459b
saksheex <saksheesinghofc@gmail.com>
2026-02-24 12:29:08 +0530

    user made calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+math_expression= input("Expression: ")
+var = math_expression.split()
+
+x = int(var[0])
+y = var[1]
+z = int(var[2])
+
+if y == "+":
+     print(f"{x+z:.1f}")
+elif y == "-":
+     print(f"{x-z:.1f}")
+elif y== "*":
+     print(f"{x*z:.1f}")
+elif y == "/":
+     print(f"{x/z:.1f}")    
