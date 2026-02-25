a05ddc85be5cad2825ed4aac189eda294595bed5
saksheex <saksheesinghofc@gmail.com>
2026-02-25 16:10:01 +0530

    check your meal time

--- meal.py
+++ meal.py
@@ -1,30 +1,30 @@
-def main():
+# def main():
 
-    time = input("What time is it?: ").casefold()
-    time = convert(time)
-    if 7 <= time <=8 :
-       print("Breakfast Time")
-    elif 12 <= time <=13 :
-        print("Lunch Time")
-    elif 18 <= time <= 19 :
-        print("Dinner Time")  
-    else:
-        print("No to for meal")     
+#     time = input("What time is it?: ").casefold()
+#     time = convert(time)
+#     if 7 <= time <=8 :
+#        print("Breakfast Time")
+#     elif 12 <= time <=13 :
+#         print("Lunch Time")
+#     elif 18 <= time <= 19 :
+#         print("Dinner Time")  
+#     else:
+#         print("No time for meal")     
 
 
-def convert(time):
+# def convert(time):
 
-    time,period = time.split()
-    hours , minutes = time.split(":")
-    hours = int(hours)
-    minutes = int(minutes)
-    if period == "am":
-        if hours == 12:
-            hours = 0
-    elif period == "pm":
-         if hours != 12:
-             hours = hours+12
-    return hours + minutes / 60     
+#     time,period = time.split()
+#     hours , minutes = time.split(":")
+#     hours = int(hours)
+#     minutes = int(minutes)
+#     if period == "am":
+#         if hours == 12:
+#             hours = 0
+#     elif period == "pm":
+#          if hours != 12:
+#              hours = hours+12
+#     return hours + minutes / 60     
     
-if __name__ == "__main__":
-     main()
+# if __name__ == "__main__":
+#      main()
