3a294fa3a65756644d7119d7c01080d07333f486
saksheex <saksheesinghofc@gmail.com>
2026-02-25 16:08:55 +0530

    WRITING IN ALL CAPS IS LIKE YELLING

--- indoor_voice.py
+++ indoor_voice.py
@@ -1,11 +1,52 @@
-def make_lower(a):
-    if any(char.isupper() for char in a):
-        print("Capital letter found, converting to lowercase\n")
-        return a.lower()
-    else:
-        print("You are good to go, no capitals found!\n")
-        return a
+# Best to use your “indoor voice” sometimes, writing entirely in lowercase.
+# In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase
+# Punctuation and whitespace should be outputted unchanged. 
 
-text = input("Enter your inputs:\n ")
-result = make_lower(text)
-print("Final Version after review:", result)  
+
+# def make_lower(a):
+#     if any(char.isupper() for char in a):
+#         print("Capital letter found, converting to lowercase\n")
+#         return a.lower()
+#     else:
+#         print("You are good to go, no capitals found!\n")
+#         return a
+
+# text = input("Enter your inputs:\n ")
+# result = make_lower(text)
+# print("Final Version after review:", result)  
+
+# user=input("Enter your input")
+# for char in user:
+#     if char.isupper():
+#         result=""
+#         result= result+"_"
+#         result= result.lower()
+
+#         print(result)
+# user=input("camelCase: ")
+# result = ""
+# for char in user:
+#     if char.isupper():          
+#         result += "_"           
+#         result += char.lower()  
+#     else:
+#         result += char          
+
+# print("snake_case:",result)
+
+
+print("Amount due: 50")
+while True:
+  if coin
+#   coin= int(input("Insert coin: "))
+  if coin==50:
+    print("change owed: 0")
+    break
+  elif coin<50:
+    print("Amount dur:" , 50-coin)
+    
+  else:
+    coin>=50
+    print("Change owed:",coin-50)  
+
+
