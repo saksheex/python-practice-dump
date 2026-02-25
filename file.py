3d9a41f393a47a5f121385e5690872484ee67551
saksheex <saksheesinghofc@gmail.com>
2026-02-25 16:07:49 +0530

    file extension type

--- file.py
+++ file.py
@@ -1,16 +1,16 @@
-user_file = input("File name: ").strip().casefold()
+# user_file = input("File name: ").strip().casefold()
 
-if user_file.endswith(".gif"):
-     print("image/gif")
-elif user_file.endswith(".jpg") or user_file.endswith(".jpeg"):
-     print("image/jpeg")
-elif user_file.endswith(".png"):
-     print("image/png")
-elif user_file.endswith(".pdf"):
-     print("application/pdf")
-elif user_file.endswith(".txt"):
-     print("text/plain")
-elif user_file.endswith(".zip"):
-     print("application/zip")
-else:
-     print("application/octet-stream") 
+# if user_file.endswith(".gif"):
+#      print("image/gif")
+# elif user_file.endswith(".jpg") or user_file.endswith(".jpeg"):
+#      print("image/jpeg")
+# elif user_file.endswith(".png"):
+#      print("image/png")
+# elif user_file.endswith(".pdf"):
+#      print("application/pdf")
+# elif user_file.endswith(".txt"):
+#      print("text/plain")
+# elif user_file.endswith(".zip"):
+#      print("application/zip")
+# else:
+#      print("application/octet-stream") 
